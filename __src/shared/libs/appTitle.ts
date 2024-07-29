f535a2ae55711952208cfeae996e54655c081323
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-07-29 14:02:52 +0500

    fix: up appTitle

--- __src/shared/libs/appTitle.ts
+++ __src/shared/libs/appTitle.ts
@@ -1,6 +1,6 @@
 const baseTitle = "Satori";
 
 export const appTitle = (title?: string): string =>
-  title && typeof title === "string" && title.trim().length === 0
+  title && typeof title === "string" && title.trim().length !== 0
     ? `${title} | ${baseTitle}`
     : baseTitle;
